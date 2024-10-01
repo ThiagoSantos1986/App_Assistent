62931ec822b3323b969d859e15b969877f9b337a
ThiagoSantos1986 <tiagomutle@hotmail.com>
2024-10-01 17:43:51 -0300

    import assembly Ai

--- app.py
+++ app.py
@@ -1,10 +1,65 @@
+import assemblyai as aai
 import os
 from gtts import gTTS
 from playsound import playsound
 import speech_recognition as sr
 import  requests
 import sys
+from  dotenv import load_dotenv
+
+#carregando as variaveis de ambiente registradas no .env
+load_dotenv()
+
+#variaveis de ambiente 
+aai.settings.api_key = os.environ['ASSEMBLYAI_API_KEY']
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "creds.json"
+
+
+def on_open(session_opened: aai.RealtimeSessionOpened):
+    print('Session_id', session_opened.session_id)
+
+
+def on_data(transcript: aai.RealtimeTranscript):
+    
+    if not transcript:
+        return
+    if isinstance( transcript, aai.RealtimeFinalTranscript):
+        print(transcript.text, end='\r\n')
+    else:
+        print(transcript.text, end='\r')
+
+def on_error(error: aai.RealtimeError):
+    print('An error', error)
+
+
+def on_close():
+    print("fechando")
+
+config = aai.TranscriptionConfig(language_code='pt', speaker_labels=True)
+trascriber  = aai.Transcriber()
+trascriber_real = aai.RealtimeTranscriber(
+    sample_rate=16_000,
+    on_data=on_data,
+    on_error=on_error,
+    on_open=on_open,
+    on_close=on_close
+
+)
+trascriber_real.connect()
+
+mic = aai.extras.MicrophoneStream(sample_rate=16_000)
+
+# transcript = trascriber.transcribe('audios/welcome.mp3', config)
+# transcript = trascriber.transcribe(mic , config)
+trascriber_real.stream(mic)
+
+trascriber_real.close()
+
+
+# for  it in transcript.utterances:
+#     print(f'Speaker {it.speaker}: {it.text}')
+
+# print(transcript.text)
 
 
 def cria_audio(audio, msg, lang="pt-br"):
@@ -14,7 +69,8 @@ def cria_audio(audio, msg, lang="pt-br"):
     os.remove(audio)
 
 
-# cria_audio('audios/welcome.mp3', "Olá eu sou Ana. Em que posso ajudar")
+# cria_audio('audios/welcome.mp3', "Olá eu sou Ana. Em que posso ajudar\
+            # qual seria seu nome? ")
 
 
 def monitora_audio():
@@ -22,8 +78,11 @@ def monitora_audio():
     with sr.Microphone() as source:
         while True:
             print('diga algo')
-            audio = recon.listen(source)            
+            audio = recon.listen(source)
+
             try:
+                recon.adjust_for_ambient_noise(source)
+                
                 msg = recon.recognize_google_cloud(audio, language="pt-BR")
                 msg = msg.lower()
                 print('voce disse: ', msg)
@@ -46,11 +105,11 @@ def executa_comandos(mensagem):
 # :if __name__ == "__main__":
     # monitora_audio()
 
-def main():
-    cria_audio('audios/welcome.mp3', "Olá eu sou Ana. Em que posso ajudar")
+# def main():
+#     # cria_audio('audios/welcome.mp3', "Olá eu sou Ana. Em que posso ajudar")
 
-    # playsound('audios/bemvindo.wav')
-    while True:
-        monitora_audio()
+#     # playsound('audios/bemvindo.wav')
+#     while True:
+#         monitora_audio()
 
-main()
+# main()
