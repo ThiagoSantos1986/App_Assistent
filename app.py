ba7aaad268556645c7acd982b3b6b4c33087051d
ThiagoSantos1986 <tiagomutle@hotmail.com>
2025-01-05 22:45:25 -0300

    NEW UPDATE

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import assemblyai as aai
+# import assemblyai as aai
+# import assemblyai as aai
 import os
 from gtts import gTTS
 from playsound import playsound
@@ -11,49 +12,49 @@ from  dotenv import load_dotenv
 load_dotenv()
 
 #variaveis de ambiente 
-aai.settings.api_key = os.environ['ASSEMBLYAI_API_KEY']
+# aai.settings.api_key = os.environ['ASSEMBLYAI_API_KEY']
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "creds.json"
 
 
-def on_open(session_opened: aai.RealtimeSessionOpened):
-    print('Session_id', session_opened.session_id)
+# def on_open(session_opened: aai.RealtimeSessionOpened):
+#     print('Session_id', session_opened.session_id)
 
 
-def on_data(transcript: aai.RealtimeTranscript):
+# def on_data(transcript: aai.RealtimeTranscript):
     
-    if not transcript:
-        return
-    if isinstance( transcript, aai.RealtimeFinalTranscript):
-        print(transcript.text, end='\r\n')
-    else:
-        print(transcript.text, end='\r')
+#     if not transcript:
+#         return
+#     if isinstance( transcript, aai.RealtimeFinalTranscript):
+#         print(transcript.text, end='\r\n')
+#     else:
+#         print(transcript.text, end='\r')
 
-def on_error(error: aai.RealtimeError):
-    print('An error', error)
+# def on_error(error: aai.RealtimeError):
+#     print('An error', error)
 
 
-def on_close():
-    print("fechando")
+# def on_close():
+#     print("fechando")
 
-config = aai.TranscriptionConfig(language_code='pt', speaker_labels=True)
-trascriber  = aai.Transcriber()
-trascriber_real = aai.RealtimeTranscriber(
-    sample_rate=16_000,
-    on_data=on_data,
-    on_error=on_error,
-    on_open=on_open,
-    on_close=on_close
+# config = aai.TranscriptionConfig(language_code='pt', speaker_labels=True)
+# trascriber  = aai.Transcriber()
+# trascriber_real = aai.RealtimeTranscriber(
+#     sample_rate=16_000,
+#     on_data=on_data,
+#     on_error=on_error,
+#     on_open=on_open,
+#     on_close=on_close
 
-)
-trascriber_real.connect()
+# )
+# trascriber_real.connect()
 
-mic = aai.extras.MicrophoneStream(sample_rate=16_000)
+# mic = aai.extras.MicrophoneStream(sample_rate=16_000)
 
 # transcript = trascriber.transcribe('audios/welcome.mp3', config)
 # transcript = trascriber.transcribe(mic , config)
-trascriber_real.stream(mic)
+# trascriber_real.stream(mic)
 
-trascriber_real.close()
+# trascriber_real.close()
 
 
 # for  it in transcript.utterances:
@@ -62,14 +63,14 @@ trascriber_real.close()
 # print(transcript.text)
 
 
-def cria_audio(audio, msg, lang="pt-br"):
+def cria_audio(audio, msg, lang="pt-BR"):
     tts = gTTS(msg, lang=lang)
     tts.save(audio)
     playsound(audio)
     os.remove(audio)
 
 
-# cria_audio('audios/welcome.mp3', "Olá eu sou Ana. Em que posso ajudar\
+cria_audio('audios/welcome.mp3', "Olá eu sou Ana. Em que posso ajudar\
             # qual seria seu nome? ")
 
 
@@ -81,7 +82,7 @@ def monitora_audio():
             audio = recon.listen(source)
 
             try:
-                recon.adjust_for_ambient_noise(source)
+                # recon.adjust_for_ambient_noise(source)
                 
                 msg = recon.recognize_google_cloud(audio, language="pt-BR")
                 msg = msg.lower()
@@ -102,13 +103,15 @@ def executa_comandos(mensagem):
     if 'fechar assistente' in mensagem:
         sys.exit()
 
-# :if __name__ == "__main__":
-    # monitora_audio()
+
+
+if __name__ == "__main__":
+    monitora_audio()
 
 # def main():
 #     # cria_audio('audios/welcome.mp3', "Olá eu sou Ana. Em que posso ajudar")
 
-#     # playsound('audios/bemvindo.wav')
+#     playsound('audios/bemvindo.wav')
 #     while True:
 #         monitora_audio()
 
